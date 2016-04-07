c128b93bffeb882d99dd6fccbc5b4a6f3d209afe
toyinvces <Cesar@macbook-pro.local>
2016-04-07 09:50:28 -0500

    Initial Commit

---
+++ PoopieFinger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoopieFinger
+//
+//  Created by Cesar Ramirez on 4/7/16.
+//  Copyright © 2016 Cesar Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
